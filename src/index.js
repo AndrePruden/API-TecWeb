25c4132322fc0dd9ec18b38e58d03d29363a8a3c
AndrePruden <andrepruden0@gmail.com>
2024-12-16 08:03:19 -0400

    Prueba

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import { envelopesRouter } from './routes/envelopes.routes.js';
 
 const app = express();
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT;  // Usamos el puerto proporcionado por el entorno de ejecución (Render en este caso)
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +13,5 @@ app.use(morgan('tiny'));
 app.use('/api/envelopes', envelopesRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);  // Este mensaje solo aparecerá cuando se ejecute localmente
 });
