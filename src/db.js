5871e61cff2c6387f1ff7a6dd7358f9d70830be8
AndrePruden <andrepruden0@gmail.com>
2024-12-16 00:43:13 -0400

    first commit

---
+++ src/db.js
@@ -0,0 +1,14 @@
+// db.js
+import mysql from 'mysql2/promise'; // Usamos mysql2 para manejar promesas
+
+// Configuración de la conexión a la base de datos
+export const pool = mysql.createPool({
+  host: 'servidorbudget.mysql.database.azure.com',
+  user: 'AndrePruden',
+  password: 'estudiante2024*',
+  database: 'budget_db',
+  port: 3306, // Puerto predeterminado de MySQL
+  ssl: {
+    rejectUnauthorized: true, // Asegúrate de tener configurado SSL si es necesario en Azure
+  },
+});
